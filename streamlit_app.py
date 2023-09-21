c9d3bd36fc688a97aac36637da5808130c85356f
yht914 <144981104+yht914@users.noreply.github.com>
2023-09-21 23:59:06 +0000

    A

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,15 @@
 # Streamlitライブラリをインポート
 import streamlit as st
-import random
 
-st.title("おみくじアプリ")
-if st.button("おみくじを引く"):
-    results=["ロシア","北朝鮮","エリトリア","アフガニスタン","ミャンマー","イエメン"]
-    result=random.choice(results)
-    st.write(f"結果：{result}")
+# 西側諸国の国名をリストに格納
+western_countries = ["アメリカ", "イギリス", "フランス", "ドイツ", "トルコ", "日本", "韓国", "イスラエル", "サウジアラビア"]
+
+# ランダムな国名を取得
+random_country = western_countries[random.randint(0, len(western_countries) - 1)]
+
+# アプリのUIを作成
+st.title("ランダムな西側諸国の国")
+st.write("ランダムに選択された国は、" + random_country + "です。")
     
 
     
