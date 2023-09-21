8fa6330220186278431b0a9300e3e6cf0a312df9
yht914 <144981104+yht914@users.noreply.github.com>
2023-09-21 04:45:54 +0000

    A

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import random
 
 st.title("おみくじアプリ")
 if st.button("おみくじを引く"):
-    results=["大吉","中吉""小吉","吉凶","大凶"]
+    results=["大吉","中吉","小吉","吉凶","大凶"]
     result=random.choice(results)
     st.write(f"結果：{result}")
     
